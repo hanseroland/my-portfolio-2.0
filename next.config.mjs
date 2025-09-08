08d8ca1422cf3976fe1f07c3c7c09729078fde33
hanseroland <rolandntougou@gmail.com>
2025-09-08 23:43:52 +0000

    feat: supppression import de datagrid

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-// next.config.mjs
-import createNextPlugin from 'next-transpile-modules';
-
-const withTM = createNextPlugin(['@mui/x-data-grid']);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-
-export default withTM(nextConfig);
+export default nextConfig;
